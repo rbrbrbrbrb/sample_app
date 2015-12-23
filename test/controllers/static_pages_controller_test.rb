71dd4f721fd0aee3cf0a8193f0ffd47e6d4be4cd
rbrbrbrbrb <rbrsanchez@gmail.com>
2015-12-23 15:40:12 +0800

    added contact page, tests pass

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -22,4 +22,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | #{@init_title}"
   end
+ 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@init_title}"
+  end
 end
