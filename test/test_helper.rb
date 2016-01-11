e009b5a75c6d2a4516b4329baa2663afed87749e
rbrbrbrbrb <rbrsanchez@gmail.com>
2016-01-11 18:55:36 +0800

    added helper tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,7 @@ Minitest::Reporters.use!
 
 
 class ActiveSupport::TestCase
+  include ApplicationHelper
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
